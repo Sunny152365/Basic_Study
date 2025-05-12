2cee97b74430b9cee268a5091e8d3c0d69ae1fa0
Sunny152365 <sonnentag0609@gmail.com>
2025-05-12 22:51:25 +0900

    Add Login_)basic_id_pw

--- Swift/Login_basic_id_pw/Login_basic_id_pw/LoginViewController.swift
+++ Swift/Login_basic_id_pw/Login_basic_id_pw/LoginViewController.swift
@@ -20,7 +20,9 @@ class LoginViewController: UIViewController {
         let id = idTextField.text ?? ""
         let pw = pwTextField.text ?? ""
 
+        // id & pw 확인
         if id == "test" && pw == "1234" {
+            // segue 스토리보드에서 하나의 화면에서 다른 화면으로 이동할 때 사용하는 연결선
             performSegue(withIdentifier: "toMain", sender: self)
         } else {
             let alert = UIAlertController(title: "로그인 실패", message: "아이디 또는 비밀번호가 틀렸습니다.", preferredStyle: .alert)
