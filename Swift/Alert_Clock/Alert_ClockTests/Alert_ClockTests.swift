7503903ac12c13a6ac67803b198d1a3d6e6ff869
Sunny152365 <sonnentag0609@gmail.com>
2025-05-14 09:15:21 +0900

    Add Alert_Clock

---
+++ Swift/Alert_Clock/Alert_ClockTests/Alert_ClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_ClockTests.swift
+//  Alert_ClockTests
+//
+//  Created by 최원일 on 5/14/25.
+//
+
+import Testing
+@testable import Alert_Clock
+
+struct Alert_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
