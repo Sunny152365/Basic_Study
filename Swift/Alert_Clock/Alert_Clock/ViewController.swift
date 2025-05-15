bac12264718b82730771e9966c00e31e05bd826d
Sunny152365 <sonnentag0609@gmail.com>
2025-05-15 09:50:41 +0900

    Add Alert_Clock

--- Swift/Alert_Clock/Alert_Clock/ViewController.swift
+++ Swift/Alert_Clock/Alert_Clock/ViewController.swift
@@ -8,81 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    // 켜진 전구 이미지, 꺼진 전구 이미지, 제거된 전구 이미지의 상수
-    let imgOn = UIImage(named: "lamp-on.png")
-    let imgOff = UIImage(named: "lamp-off.png")
-    let imgRemove = UIImage(named: "remove.png")
-    
-    // 전구 상태를 나타내는 변수(켜진 상태는 true, 꺼진 상태는 false)
-    var isLampOn = true
+    // 타이머가 구동되면 실행할 함수
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    // 타이머 간격. 1초
+    let interval = 1.0
+    // 타이머가 설정한 간격대로 실행되는지 확인하기 위한 변수
+    var count = 0
+    var alarmTime: String?
+    var alertFlag = false       // 알람 기능 설정
 
-    @IBOutlet weak var lampImg: UIImageView!
+    // 현재 시간 레이블의 아웃렛 변수
+    @IBOutlet var lblCurrentTime: UILabel!
+    // 선택 시간 레이블의 아웃렛 변수
+    @IBOutlet var lblPickerTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        // 타이머 설정
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        // 전달된 인수 저장
+        let datePickerView = sender
         
-        // 앱 실행 시 켜진 전구 이미지가 나타남
-        lampImg.image = imgOn
+        // DateFormatter 클래스 상수 선언
+        let formatter = DateFormatter()
+        // formatter의 dateFormat 속성을 설정
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        // 데이트 피커에서 선택한 날짜를 formatter의 dateFormat에서 설정한 포맷대로 string 메서드를 사용하여 문자열(String)로 변환
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
     }
+    
+    // 타이머가 구동된 후 정해진 시간이 되었을 때 실행할 함수
+    @objc func updateTime() {
+        // count 값을 문자열로 변환하여 lblCurrentTime.text에 출력
+        // lblCurrentTime.text = String(count)
+        // count = count + 1        // count 값을 1 증가
+        
+        // 현재 시간을 가져옴
+        let date = NSDate()
 
-    @IBAction func btnLampOn(_ sender: UIButton) {
-        if(isLampOn==true) {
-            // 전구가 켜져 있을 경우, 전구가 켜져 있다고 알려 주는 얼럿을 실행함
-            let lampOnAlert = UIAlertController(title: "경고", message: "현재 On 상태입니다", preferredStyle: UIAlertController.Style.alert)
-            let onAction = UIAlertAction(title: "네, 알겠습니다.", style: UIAlertAction.Style.default, handler: nil)
-            lampOnAlert.addAction(onAction)
-            present(lampOnAlert, animated: true, completion: nil)
-        } else { // 전구가 켜져 있지 않을 경우, 전구를 켬
-            lampImg.image = imgOn
-            isLampOn=true
+        // 상수 formatter의 dateFormat 속성을 설정
+        let formatter = DateFormatter()
+        // 현재 날짜(date)를 formatter의 dateFormat에서 설정한 포맷대로 string 메서드를 사용하여 문자열(String)로 변환
+        // 문자열로 변환한 date 값을 "현재시간:"이라는 문자열에 추가
+        // 그리고 그 문자열을 lblCurrentTime의 text에 입력
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간 : " + formatter.string(from: date as Date)
+
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if (alarmTime == currentTime) {
+            if !alertFlag {
+                let lampOnAlert = UIAlertController(title: "알림", message: "설정된 시간입니다.!!", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "네, 확인했습니다.", style: UIAlertAction.Style.default, handler: nil)
+                lampOnAlert.addAction(onAction)
+                present(lampOnAlert, animated: true, completion: nil)
+                alertFlag = true
+            }
+            view.backgroundColor = UIColor.red
+        }
+        else {
+            alertFlag = false
+            view.backgroundColor = UIColor.white
         }
     }
-    
-    @IBAction func btnLampOff(_ sender: UIButton) {
-        if isLampOn {
-            // 전구가 켜져 있을 경우, 끌 것인지 묻는 얼럿을 실행함
-            let lampOffAlert = UIAlertController(title: "램프 끄기", message: "램프를 끄시겠습니까?", preferredStyle: UIAlertController.Style.alert)
-            
-            let offAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default, handler: {
-                ACTION in self.lampImg.image = self.imgOff
-                self.isLampOn=false
-            })
-            let cancelAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.default, handler: nil)
-            
-            lampOffAlert.addAction(offAction)
-            lampOffAlert.addAction(cancelAction)
-            
-            present(lampOffAlert, animated: true, completion: nil)
-        }
-    }
-    
-    // 전구를 제거할 것인지 묻고 '켜기', '끄기', '제거'의 세 가지 동작 중에서 선택해 실행
-    @IBAction func btnLampRemove(_ sender: UIButton) {
-        let lampRemoveAlert = UIAlertController(title: "램프 제거", message: "램프를 제거하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
-
-         let offAction = UIAlertAction(title: "아니오, 끕니다(off).", style: UIAlertAction.Style.default, handler: {
-             ACTION in self.lampImg.image = self.imgOff
-             self.isLampOn=false
-         })
-         let onAction = UIAlertAction(title: "아니오, 켭니다(on).", style: UIAlertAction.Style.default) {
-             ACTION in self.lampImg.image = self.imgOn
-             self.isLampOn=true
-         }
-         let removeAction = UIAlertAction(title: "네, 제거합니다.", style: UIAlertAction.Style.destructive, handler: {
-             ACTION in self.lampImg.image = self.imgRemove
-             self.isLampOn=false
-         })
-
-         lampRemoveAlert.addAction(offAction)
-         lampRemoveAlert.addAction(onAction)
-         lampRemoveAlert.addAction(removeAction)
-
-         present(lampRemoveAlert, animated: true, completion: nil)
-    }
-    
 }
+
+
+
+
 
 // 익명함수(Anonymous Function), 클로저(Closure)
 // 함수 이름을 선언하지 않고 바로 함수 몸체만 만들어 사용하는 일회용 함수
