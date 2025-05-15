4804bbb7808d67e6ee2141bc5c8c1fdbdbad74a3
Sunny152365 <sonnentag0609@gmail.com>
2025-05-15 14:37:39 +0900

    Add WebView_App

---
+++ Swift/WebView_App/WebView_AppTests/WebView_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_AppTests.swift
+//  WebView_AppTests
+//
+//  Created by 최원일 on 5/15/25.
+//
+
+import Testing
+@testable import WebView_App
+
+struct WebView_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
