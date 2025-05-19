43611d191c8665f1660d19fd63d1fed6e7c1e412
Sunny152365 <sonnentag0609@gmail.com>
2025-05-19 20:55:45 +0900

    Add MapView First

---
+++ Swift/MapView/MapView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by 최원일 on 5/19/25.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myMap: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift/MapView/MapViewTests/MapViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewTests.swift
+//  MapViewTests
+//
+//  Created by 최원일 on 5/19/25.
+//
+
+import Testing
+@testable import MapView
+
+struct MapViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
