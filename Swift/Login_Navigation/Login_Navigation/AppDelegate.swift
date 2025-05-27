ccdcf5fead06c777b0e515f86b74eb1a22ee6656
Sunny152365 <sonnentag0609@gmail.com>
2025-05-27 23:51:53 +0900

    First Login_Navigation

--- Swift/Login_Navigation/Login_Navigation/AppDelegate.swift
+++ Swift/Login_Navigation/Login_Navigation/AppDelegate.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let naverLoginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 네이버 로그인 초기 설정
+                naverLoginInstance?.isNaverAppOauthEnable = true      // 네이버 앱으로 로그인 허용
+                naverLoginInstance?.isInAppOauthEnable = true         // 인앱 브라우저 로그인 허용
+                naverLoginInstance?.serviceUrlScheme = "naverlogin"  // Info.plist에 등록한 URL Scheme와 같아야 함
+                naverLoginInstance?.consumerKey = "YOUR_CONSUMER_KEY"        // 네이버 개발자센터 Client ID
+                naverLoginInstance?.consumerSecret = "YOUR_CONSUMER_SECRET"  // 네이버 개발자센터 Client Secret
+                naverLoginInstance?.appName = "YOUR_APP_NAME"                // 앱 이름
+
+        
         return true
     }
 
@@ -23,7 +34,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        
     }
+    
+    func application(_ app: UIApplication, open url: URL,
+                         options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            if naverLoginInstance?.application(app, open: url, options: options) ?? false {
+                return true
+            }
+            return false
+        }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
--- Swift/Login_Navigation/Login_Navigation/LoginPageViewController.swift
+++ Swift/Login_Navigation/Login_Navigation/LoginPageViewController.swift
@@ -14,8 +14,15 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+// Apple 로그인 관련 프레임워크
+import AuthenticationServices
+// 네이버 로그인 SDK
+import NaverThirdPartyLogin
 
+class LoginViewController: UIViewController {
+    
+    let naverLoginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
+    
     /// 기본적으로 생기는 네비게이션 왼쪽 아이템 숨기기
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -25,30 +32,86 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var appleLoginContainerView: UIView!
+    @IBOutlet weak var naverLoginButton: UIView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAppleLoginButton()
+        /*
+        // 네이버 로그인 버튼 커스터마이징 예시
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleNaverLogin))
+        naverLoginButton.addGestureRecognizer(tapGesture)
+        
+        // 네이버 로그인 인스턴스 초기화
+        naverLoginInstance.delegate = self
+        
+        // 뷰 스타일도 원하면 추가
+        naverLoginButton.backgroundColor = UIColor(red: 3/255, green: 199/255, blue: 90/255, alpha: 1) // 네이버 그린
+        naverLoginButton.layer.cornerRadius = 5
+        */
+    }
+    /*
+    // 네이버 로그인 파트
+    @objc func handleNaverLogin() {
+        print("네이버 로그인 뷰 탭됨")
+        naverLoginInstance.requestThirdPartyLogin()
     }
 
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("네이버 로그인 성공")
+        // 토큰 받아서 처리
+    }
+
+    func oauth20ConnectionDidFailWithError(_ error: Error) {
+        print("네이버 로그인 실패: \(error.localizedDescription)")
+    }
+     */
+    // 애플 로그인 파트
+    private func setupAppleLoginButton() {
+        let appleButton = ASAuthorizationAppleIDButton()
+        appleButton.frame = appleLoginContainerView.bounds
+        appleButton.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        appleButton.addTarget(self, action: #selector(handleAppleLogin), for: .touchUpInside)
+        appleLoginContainerView.addSubview(appleButton)
+    }
+
+    @objc private func handleAppleLogin() {
+        let alert = UIAlertController(title: "애플 로그인", message: "선택되었습니다.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+
+        // 유료 계정 없어도 여기까지는 OK
+        // 이후 인증 요청은 유료 계정 있어야 작동함
+    }
+
+    
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         let id = idTextField.text ?? ""
         let pw = pwTextField.text ?? ""
-
-        print("✅ loginButtonTapped 호출됨")
+        
         if id == "test" && pw == "1234" {
-            performSegue(withIdentifier: "MyPageAfterLoginViewController", sender: self)
-            print("로그인 버튼 눌림")
-            print("입력된 ID: \(id), PW: \(pw)")
-
-        } else {
-            let alert = UIAlertController(title: "로그인 실패", message: "아이디 또는 비밀번호가 틀렸습니다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
+            // 250527 현재 여기서 디버그 발생
+            // performSegue(withIdentifier: "MyPageAfterLoginViewController", sender: self)
+            // 코드로 직접 뷰컨트롤러 인스턴스 생성 후 push (스토리보드 segue는 별개로 작동)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let myPageVC = storyboard.instantiateViewController(withIdentifier: "MyPageAfterLoginViewController") as? MyPageAfterLoginViewController {
+                self.navigationController?.pushViewController(myPageVC, animated: true)
+                }
+            
+            } else {
+                let alert = UIAlertController(title: "로그인 실패", message: "아이디 또는 비밀번호가 틀렸습니다.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+            }
         }
-    }
     @IBAction func BackButton(_ sender: UIButton) {
+        print("Button tapped!")
         // 현재 뷰에서, 이전 뷰로 돌아가고 싶을 때
         self.navigationController?.popViewController(animated: true)
     }
-
+        
 }
+
